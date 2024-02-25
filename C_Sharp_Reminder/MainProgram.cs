e83e313a7a7ee998bac7edb090446561a82cb014
Alexandr Pekel <119462299+RaphaelsHub@users.noreply.github.com>
2024-02-25 23:01:14 +0200

    Collections

--- C_Sharp_Reminder/MainProgram.cs
+++ C_Sharp_Reminder/MainProgram.cs
@@ -1,5 +1,6 @@
 ﻿namespace C_Sharp_Reminder;
-
+using System;
+using System.Collections.Generic;
 // Using "internal" with vars, methods, classes - doesn't give permission to use them in other programs, projects, assemblies.
 internal class MainProgram
 {
@@ -11,13 +12,99 @@ internal class MainProgram
         StringView();
         CreatingArray();
         Parsing();
+        CreatingList();
+        CreatingVector();
+        CreatingQueue();
+        CreatingStack();
+        CreatingDictionary();
+    }
+
+    private static void CreatingDictionary()
+    {
+        var dictionary = new Dictionary<int, string>();
+        dictionary.Add(1,"hELLO");
+        dictionary.Add(2,"hELLO");
+        dictionary.Add(3,"hELLO");
+
+        dictionary = new Dictionary<int, string>()
+        {
+            { 1, "john" },
+            { 2, "john" },
+            { 3, "john" }
+        };
+
+        string name = dictionary[2];
+        Console.WriteLine(name);
+    }
+
+    private static void CreatingStack()
+    {
+        
+    }
+
+    private static void CreatingQueue()
+    {
+         
+    }
+
+    private static void CreatingVector() // Vector in c++ is a list in c#
+    {
+        var a = new List<int>() { 2, 5, 6, 7, 8, 9 };
+        a.Add(3);
+        foreach (var vas in a)
+        {
+            Console.Write(vas+" ");
+        }
+        Console.WriteLine();
+        
+        a.Sort();
+        
+        foreach (var vas in a)
+        {
+            Console.Write(vas+" ");
+        }
+    }
+
+    private static void CreatingList()  // 
+    {
+        //Это позволит вам добавить узел в список в нужное место, а также обеспечит доступ к методам для работы с узлами (например, Next, Previous, Value).
+        LinkedList<int> a = new LinkedList<int>();
+        a.AddLast(2);
+        a.AddLast(2);
+        a.AddLast(2);
+        a.AddLast(2);
+        a.AddLast(2);
+        a.AddLast(2);
+        a.AddLast(2);
+        a.AddLast(2);
+
+        if (a.First != null)
+        {
+            LinkedListNode<int> firstNode = a.First;
+            if (firstNode.Next != null) firstNode.Next.Value = 3;
+        } //Работа с нодой
+
+
+        foreach (var vas in a)
+        {
+            Console.Write(vas+" ");
+        }
+        Console.WriteLine();
+        
+        a.RemoveFirst();
+        
+        foreach (var vas in a)
+        {
+            Console.Write(vas+" ");
+        }
+        Console.WriteLine();
     }
 
     private static void Parsing()
     {
         double num = 10.5;
         int roundedNum = (int)Math.Round(num, MidpointRounding.ToZero);
-        Console.WriteLine(roundedNum);
+        Console.WriteLine("\n"+roundedNum);
         roundedNum = (int)Math.Round(num, MidpointRounding.AwayFromZero);
         Console.WriteLine(roundedNum);
     }
@@ -25,8 +112,22 @@ internal class MainProgram
     private static void CreatingArray()
     {
         int[] arr = new int[5];
-        int[] arr1 = new int[5] {2,4,6,2,5};
+        int[] arr1 = new int[5] {2,4,6,7,8};
         //int[] arr2 = {2,4,6,2,5};
+        foreach (var vas in arr1)
+        {
+            Console.Write(vas+" ");
+        }
+        Console.WriteLine();
+
+        int index = Array.BinarySearch(arr1, 4);
+        Console.WriteLine(arr1[index] + " Is our num");
+        
+        Array.Copy(arr1,arr,arr1.Length);
+        foreach (var vas in arr)
+        {
+            Console.Write(vas+" ");
+        }
     }
 
     private static void StringView()
