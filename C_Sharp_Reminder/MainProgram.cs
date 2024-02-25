d4adbaa942520e49c8fb8e80fb36932ef70636d1
Alexandr Pekel <119462299+RaphaelsHub@users.noreply.github.com>
2024-02-25 17:44:37 +0200

    Concatination + creating array + math.round

--- C_Sharp_Reminder/MainProgram.cs
+++ C_Sharp_Reminder/MainProgram.cs
@@ -8,22 +8,59 @@ internal class MainProgram
         CheckOnOverFlow();
         Concatenation();
         ConsoleView();
+        StringView();
+        CreatingArray();
+        Parsing();
+    }
+
+    private static void Parsing()
+    {
+        double num = 10.5;
+        int roundedNum = (int)Math.Round(num, MidpointRounding.ToZero);
+        Console.WriteLine(roundedNum);
+        roundedNum = (int)Math.Round(num, MidpointRounding.AwayFromZero);
+        Console.WriteLine(roundedNum);
+    }
+
+    private static void CreatingArray()
+    {
+        int[] arr = new int[5];
+        int[] arr1 = new int[5] {2,4,6,2,5};
+        //int[] arr2 = {2,4,6,2,5};
+    }
+
+    private static void StringView()
+    {
+        var str = string.Concat("Alex ", "Pekel");
+        Console.WriteLine(str);
+        str = string.Join(" ","Alex", "Pekel", "Is A King");
+        Console.WriteLine(str);
+        str = str.Insert(0, "By the way, ");
+        Console.WriteLine(str);
+        str = str.Remove(10);
+        Console.WriteLine(str);
+        str = str.Replace('a', 'E');
+        Console.WriteLine(str);
+        string []strings = str.Split(';'); //Разделит на под массивы и вернет массивы
+        Console.WriteLine(strings[0]);
+        str = str.ToLower();
+        Console.WriteLine(str);
     }
 
     private static void ConsoleView()
     {
         Console.ForegroundColor = ConsoleColor.White; //sets the color text
         Console.BackgroundColor = ConsoleColor.Black; //sets the color backGround
-        
+
         Console.WriteLine("Hello World!");
         Console.Write("What's your name: ");
 
         var sequenceOfCharacters = Console.ReadLine()!;
-        
+
         Console.WriteLine($"{sequenceOfCharacters}, nice to meet you!");
 
-        var number = Console.Read();
-        Console.WriteLine($"You entered: {(char)number}, Its code is: {number}");
+        // var number = Console.Read();
+        // Console.WriteLine($"You entered: {(char)number}, Its code is: {number}");
     }
 
     private static void CheckOnOverFlow()
@@ -40,16 +77,20 @@ internal class MainProgram
 
     private static void Concatenation()
     {
-        string name = "Alex";
-        string surname = "Pekel";
-        string fullName1 = name + " " + surname;
-        string fullName2 = string.Concat(name, " ", surname);
-        string fullName3 = string.Format("{0} {1}", name, surname);
-        string fullName4 = $"{name} {surname}";
-        
+        var name = "Alex";
+        var surname = "Pekel";
+        var fullName1 = name + " " + surname;
+        var fullName2 = string.Concat(name, " ", surname);
+        var fullName3 = string.Format("{0} {1}", name, surname);
+        var fullName4 = $"{name} {surname}";
+
+        var subString = fullName1.Substring(5);
+
         Console.WriteLine(fullName1);
         Console.WriteLine(fullName2);
         Console.WriteLine(fullName3);
         Console.WriteLine(fullName4);
+        Console.WriteLine(subString);
+        Console.WriteLine($"{string.IsNullOrEmpty("Ak")} {string.IsNullOrWhiteSpace(null)}");
     }
 }
